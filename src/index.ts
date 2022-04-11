7aa6af4fb9abcca3e4eefab0eec7e08f10df0cd8
YuyaIida <i12.yuya.i15@gmail.com>
2022-04-11 11:42:08 +0900

    fix: commit

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
-import { apiClient } from 'src/lib'
+// import { apiClient } from 'src/lib'
+import aspida from '@aspida/axios'
+import axios from 'axios'
+import api from 'src/api/$api'
 
+const apiClient = api(aspida(axios, { baseURL: 'https://hobby.microcms.io/api/v1/' }))
 const articleId = 'xg8qvl87pey'
 const res = apiClient.articles._articleId(articleId).$get()
 
-console.log(res)
+console.log(apiClient)
--- src/lib/apiClient.ts
+++ src/lib/apiClient.ts
@@ -1,5 +1,7 @@
-import aspida from '@aspida/axios'
-import axios from 'axios'
-import api from 'src/api/$api'
+// import aspida from '@aspida/axios'
+// import axios from 'axios'
+// import api from 'src/api/$api'
 
-export const apiClient = api(aspida(axios, { baseURL: 'https://hobby.microcms.io/api/v1/' }))
+// export const apiClient = api(aspida(axios, { baseURL: 'https://hobby.microcms.io/api/v1/' }))
+
+export const apiClient = 'hoge'
